bf9fc33f7deac9c4f0f5786ebb6d4e4f084747d5
Wildan Firdaus <firdaus@alterra.id>
2020-01-08 11:43:16 +0700

    refined login feature

--- src/pages/Profil.jsx
+++ src/pages/Profil.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Redirect } from "react-router-dom";
-import {Container, Form, Row, Col} from 'react-bootstrap';
+import {Container, Form, Row, Col, ListGroup} from 'react-bootstrap';
 import NavigationBar from "../components/navbar";
 
 
@@ -17,20 +17,24 @@ const Profil = props => {
                 <Container fluid={true}>
                     <Container className="mt-5">
                         <Row>
-                            <Form className="mx-auto">
-                                <Form.Group as={Row}>
-                                    <Form.Label column sm="2">Email</Form.Label>
-                                    <Col sm="10">
-                                        <Form.Control plaintext readOnly defaultValue={email} />
-                                    </Col>
-                                </Form.Group>
-                                <Form.Group as={Row}>
-                                    <Form.Label column sm="2">Full name</Form.Label>
-                                    <Col sm="10">
-                                        <Form.Control plaintext readOnly defaultValue={fullname} />
-                                    </Col>
-                                </Form.Group>
-                            </Form>
+                            <ListGroup className="mx-auto">
+                                <ListGroup.Item>
+                                    <Form.Group as={Row} className="mb-0">
+                                        <Form.Label column sm="4">Email</Form.Label>
+                                        <Col sm="8">
+                                            <Form.Control plaintext readOnly defaultValue={": "+email} />
+                                        </Col>
+                                    </Form.Group>
+                                </ListGroup.Item>
+                                <ListGroup.Item>
+                                    <Form.Group as={Row} className="mb-0">
+                                        <Form.Label column sm="4">Full name</Form.Label>
+                                        <Col sm="8">
+                                            <Form.Control plaintext readOnly defaultValue={": "+fullname} />
+                                        </Col>
+                                    </Form.Group>
+                                </ListGroup.Item>
+                            </ListGroup>
                         </Row>
                     </Container>
                 </Container>
